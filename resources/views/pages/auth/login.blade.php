d9d43e939feaf603f8eaddd9d1bb5ea38344dc5f
Bagos Hadi Wijaya <bagoshadi123@gmail.com>
2024-05-08 21:52:19 +0700

    3

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -49,7 +49,7 @@
       </div>
     </div>
     <div class="text-center mt-5 text-muted">
-      Copyright &copy; 2022 &mdash; Mulai Dari Null
+      Copyright &copy; 2024
     </div>
   </div>
   <script>
